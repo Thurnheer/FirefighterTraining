1947e1b7551aea0f009b1caee9f78ddacc22ef99
Christoph Thurnheer <c.thurnheer@gmx.ch>
2019-12-21 18:04:56 +0100

    clean up JahreskalenderParser

--- FirefighterTrainingPlaner/middleware/JahreskalenderParser.cpp
+++ FirefighterTrainingPlaner/middleware/JahreskalenderParser.cpp
@@ -16,17 +16,12 @@
 
 //------------------------------------------------------------------------------------------------
 //
-const QString JahreskalenderParser::TAG("tag");
-const QString JahreskalenderParser::BRANDHAUS("brandhaus");
-
 //------------------------------------------------------------------------------------------------
 //
 JahreskalenderParser::JahreskalenderParser(QObject *parent)
 : QObject(parent)
 , m_currentDivision(SqlTableNames::eZug)
 {
-    m_indicatorWords.push_back(TAG);
-    m_indicatorWords.push_back(BRANDHAUS);
 }
 
 //------------------------------------------------------------------------------------------------
@@ -73,253 +68,7 @@ void JahreskalenderParser::importCalendar(const QUrl& path)
     parseEvents(xlsx, exerciseSpezialisten, SqlTableNames::eSpezialisten);
     parseEvents(xlsx, sonstiges, SqlTableNames::eSonstiges);
 
-/*    xlsx.selectSheet("Themen");
-    importDrills(xlsx);
-    importCourse(xlsx);
-    xlsx.selectSheet("Farbcode");
-    getEventColors(xlsx);
-    xlsx.selectSheet("Kalender 2019");
-    importEvents(xlsx);*/
     emit parsed();
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::getEventColors(const QXlsx::Document &xlsx)
-{
-    int row = 2;
-    int col = 1;
-    QString name;
-    do
-    {
-        name = xlsx.read(row, col).toString();
-        QXlsx::Cell* cell = xlsx.cellAt(row, col+1);
-        if(cell && cell->format().hasFillData())
-        {
-            m_colorIndexMap.push_back(cell->format().fillIndex());
-        }
-        ++row;
-    }
-    while(!name.isEmpty());
-}
-
-//------------------------------------------------------------------------------------------------
-//
-bool JahreskalenderParser::hasColor(int color)
-{
-    for(size_t i = 0; i < m_colorIndexMap.size(); ++i)
-    {
-        if(m_colorIndexMap[i] == color)
-            return true;
-    }
-    return false;
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::importEvents(const QXlsx::Document& xlsx)
-{
-    for(int ii = 0; ii < 12; ++ii)
-    {
-        int i = ii % 6;
-        int k = 0;
-        if(ii >= 6)
-        {
-            k = 33;
-        }
-        for(int j = 3+k; j < 34+k; ++j)
-        {
-            QString name = xlsx.read(j, i*4+3).toString();
-            if(!name.isEmpty())
-            {
-                if(xlsx.cellAt(j, i*4+3))
-                {
-                    QXlsx::Format format = xlsx.cellAt(j, i*4+3)->format();
-                    format.formatKey();
-                    if(format.hasFillData() && hasColor(format.fillIndex()))
-                    {
-                        parseEvent(name, QDate(2019,ii+1, j-k-2));
-                    }
-                }
-            }
-        }
-    }
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::parseEvent(const QString &name, const QDate &inDate)
-{
-    QStringList events = name.split("//", QString::SkipEmptyParts);
-    QStringList::iterator it;
-    for(it = events.begin();it != events.end(); ++it)
-    {
-        if((*it).contains("A/B/C", Qt::CaseInsensitive))
-        {
-            (*it).replace((*it).indexOf("A/B/C"),5,"a-c");
-        }
-        if((*it).contains("MD alle", Qt::CaseInsensitive) || (*it).contains("a-c", Qt::CaseInsensitive) || (*it).contains("MD Parkdienst", Qt::CaseInsensitive))
-        {
-            int start = (*it).indexOf(" ");
-            int len = (*it).length() - start;
-            if(!(*it).contains("MD Parkdienst", Qt::CaseInsensitive))
-            {
-                len   = (*it).lastIndexOf(QRegExp("(\\d+)")) - start;
-            }
-            else
-            {
-                (*it) += "3";
-            }
-            (*it).replace(start, len, " A/");
-            generateEvent(*it, inDate);
-            (*it).replace((*it).lastIndexOf("A"), 1, "B");
-            generateEvent(*it, inDate);
-            (*it).replace((*it).lastIndexOf("B"), 1, "C");
-            generateEvent(*it, inDate);
-        }
-        else if((*it).contains("alle", Qt::CaseInsensitive))
-        {
-            int start = 0;
-            int len   = (*it).lastIndexOf(QRegExp("(\\d+)")) - start;
-            (*it).replace(start, len, "Zug 1/");
-            generateEvent(*it, inDate);
-            (*it).replace((*it).lastIndexOf("Zug 1"), 5, "Zug 2");
-            generateEvent(*it, inDate);
-            (*it).replace((*it).lastIndexOf("Zug 2"), 5, "Zug 3");
-            generateEvent(*it, inDate);
-            (*it).replace((*it).lastIndexOf("Zug 3/"), 6, "K-Zug ");
-            generateEvent(*it, inDate);
-        }
-        else
-        {
-            generateEvent(*it, inDate);
-        }
-    }
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::generateEvent(const QString &name, const QDate &inDate)
-{
-    Event event;
-    event.setName(name);
-    event.setUuid(QUuid::createUuid().toString());
-    setEventType(event);
-    setEventStartEndTime(event, inDate);
-    event.save();
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::setEventType(Event &event)
-{
-    if(event.name().startsWith("Zug") && !event.name().startsWith("Zugsr", Qt::CaseInsensitive) && !event.name().contains("Zugführer", Qt::CaseInsensitive))
-    {
-        event.setEventType(SqlTableNames::eZug);
-    }
-    else if(event.name().contains("MD", Qt::CaseSensitive) ||
-            event.name().contains("Masch", Qt::CaseSensitive) ||
-            event.name().contains("Fahrer", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::eMaschinistenHRB);
-    }
-    else if(event.name().contains("AS", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::eAtemschutz);
-    }
-    else if(event.name().contains("K-Zug", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::eKommandoZug);
-    }
-    else if(event.name().contains("Piket", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::ePikettzug);
-    }
-    else if(event.name().contains("Abst", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::eAbsturzsicherung);
-    }
-    else if(event.name().contains("Kader", Qt::CaseSensitive) || event.name().contains("Zugsrapport"))
-    {
-        event.setEventType(SqlTableNames::eKader);
-    }
-    else if(event.name().contains("UOF", Qt::CaseInsensitive))
-    {
-        event.setEventType(SqlTableNames::eUof);
-    }
-    else if(event.name().contains("Off", Qt::CaseInsensitive) || event.name().contains("was ist neu", Qt::CaseInsensitive))
-    {
-        event.setEventType(SqlTableNames::eOff);
-    }
-    else if(event.name().contains("Jahresplanung", Qt::CaseInsensitive) || event.name().contains("Zugführer", Qt::CaseInsensitive))
-    {
-        event.setEventType(SqlTableNames::eZugfuehrer);
-    }
-    else if(event.name().contains("Spezialisten", Qt::CaseSensitive))
-    {
-        event.setEventType(SqlTableNames::eSpezialisten);
-    }
-    else
-    {
-        event.setEventType(SqlTableNames::eUnknown);
-    }
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::setEventStartEndTime(Event &event, const QDate &dateWithoutTime)
-{
-    QDateTime date;
-    date.setDate(dateWithoutTime);
-    QStringList::iterator it;
-    QSqlQuery query;
-    int index = event.name().lastIndexOf(QRegExp("\\d"));
-    QString category;
-    if(index > 0)
-    {
-        QString number = event.name().at(index);
-        QString tablename = m_tableNames.m_tableNames[(SqlTableNames::DivisionT)event.eventType()];
-        if(!tablename.isEmpty())
-        {
-            if(query.exec(QString::fromLatin1("SELECT * FROM %1 WHERE number == %2").arg(tablename, number)))
-            {
-                query.next();
-                category = query.value("category").toString();
-            }
-        }
-    }
-
-    for(it = m_indicatorWords.begin(); it != m_indicatorWords.end(); ++it)
-    {
-        if(category.contains(*it, Qt::CaseInsensitive) || event.name().contains(*it, Qt::CaseInsensitive)
-                || dateWithoutTime.dayOfWeek() == 6/*Saturday*/)
-        {
-            date.setTime(QTime(0, 0, 1));
-            event.setStartDate(date);
-            date.setTime(QTime(23, 59, 59));
-            event.setEndDate(date);
-            return;
-        }
-    }
-    if(event.eventType() == m_tableNames.eAtemschutz)
-    {
-        date.setTime(QTime(19, 15));
-        event.setStartDate(date);
-        date.setTime(QTime(22, 15));
-        event.setEndDate(date);
-        return;
-    }
-    date.setTime(QTime(19, 15));
-    event.setStartDate(date);
-    date.setTime(QTime(21, 15));
-    event.setEndDate(date);
-    if(event.name().contains("Hauptübung"))
-    {
-        date.setTime(QTime(18,00));
-        event.setStartDate(date);
-        date.setTime(QTime(23,00));
-        event.setEndDate(date);
-    }
 }
 
 //------------------------------------------------------------------------------------------------
@@ -408,19 +157,12 @@ void JahreskalenderParser::generateDrillTable(const QString &tablename)
 
 //------------------------------------------------------------------------------------------------
 //
-QString JahreskalenderParser::getDrillTeacher(QString &toParse)
-{
-    return "";
-}
-
-//------------------------------------------------------------------------------------------------
-//
 void JahreskalenderParser::parseDrill(int number, const QString& category, const QString& drillDescription)
 {
     QSqlQuery query;
     QString teacher;
     QString description = drillDescription;
-    teacher = getDrillTeacher(description);
+    //teacher = getDrillTeacher(description);
     if(teacher.isEmpty())
     {
         teacher = " ";
@@ -435,18 +177,4 @@ void JahreskalenderParser::parseDrill(int number, const QString& category, const
             qDebug() << query.lastError();
         }
     }
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::importCourse(const QXlsx::Document &xlsx)
-{
-
-}
-
-//------------------------------------------------------------------------------------------------
-//
-void JahreskalenderParser::parseCourse()
-{
-
 }
--- FirefighterTrainingPlaner/middleware/JahreskalenderParser.hpp
+++ FirefighterTrainingPlaner/middleware/JahreskalenderParser.hpp
@@ -25,33 +25,16 @@ signals:
 
 private:
 
-    void getEventColors(const QXlsx::Document &xlsx);
-    bool hasColor(int color);
-
-    void importEvents(const QXlsx::Document &xlsx);
-    void parseEvent(const QString& name, const QDate& inDate);
-    void generateEvent(const QString& name, const QDate& date);
-    void setEventType(Event& event);
-    void setEventStartEndTime(Event& event, const QDate &dateWithoutTime);
-
     void importDrills(const QXlsx::Document& xlsx);
     void parseDrill(int number, const QString &category, const QString &drillDescription);
     void generateDrillTable(const QString& tablename);
     QStringList getCompositedDrills(const QString& name);
-    QString getDrillTeacher(QString& toParse);
 
-    void importCourse(const QXlsx::Document& xlsx);
-    void parseCourse();
-
-    std::vector<int> m_colorIndexMap;
-    QStringList m_indicatorWords;
     SqlTableNames::DivisionT     m_currentDivision;
     SqlTableNames m_tableNames;
 
 
     // indicator words
-    static const QString TAG;
-    static const QString BRANDHAUS;
     void parseEvents(const QXlsx::Document &xlsx, QVector<QRegularExpression> names, SqlTableNames::DivisionT eventType);
 };
 
